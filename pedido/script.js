a5b115568e51404b35a833a02f785896f67345c2
Yago09071 <yagonegrao09@gmail.com>
2024-03-07 17:02:18 -0300

    joia

--- pedido/script.js
+++ pedido/script.js
@@ -1,24 +1,29 @@
-function comecar(){
+var totalGlobal;
+
+function comecar() {
     var nome = document.getElementById("exampleFormControlInput1").value;
-    show(nome);
+    show(nome)
 }
 
-function show(nome){
+function show(nome) {
     document.getElementById("nomePrint").textContent = nome;
-    var div = document.getElementById("selects-div")
-    if(nome && nome.trim !==""){
-        div.style.display = "flex"
-        document.getElementById("exampleFormControlInput1").style.background = "none"
-    }else{
-        document.getElementById("error").style.display = "block"
+    var div = document.getElementById("selects-div");
+    var zap = document.getElementById("zap");
+    if (nome && nome.trim() !== "") {
+        div.style.display = "flex";
+        zap.style.display = "flex";
+        document.getElementById("exampleFormControlInput1").style.background = "nome";
+    } else {
+        document.getElementById("error").style.display = "flex";
     }
-   
 }
 
-function fechar(){
-    document.getElementById("error").style.display ="none"
-    document.getElementById("exampleFormControlInput1").style.display = "lightpink";
+
+function fechar() {
+    document.getElementById("error").style.display = "none";
+    document.getElementById("exampleFormControlInput1").style.background = "none";
 }
+   
 
 function calcularTotal(){
     var selectAA = document.getElementById("select-anti-alergico");
@@ -36,119 +41,119 @@ function calcularTotal(){
     var I = 0;
     
     switch (selectAA.value) {
-        case "Remedio1AA":
-            total =  total + 119,90;
-            AA = 119,90;
+        case "Fluconozema":
+            total =  total + 119.90;
+            AA = 119.90;
             break;
-            case "Remedio2AA":
-            total =  total + 29,90;
-            AA = 29,90;
+            case "Gastrote":
+            total =  total + 29.90;
+            AA = 29.90;
             break;
-            case "Remedio3AA":
-            total =  total + 59,90;
-            AA = 59,90;
+            case "Colflex":
+            total =  total + 59.90;
+            AA = 59.90;
             break;
-            case "Remedio4AA":
-            total =  total + 226,90;
-            AA = 226,90;
+            case "CaldêMDK":
+            total =  total + 226.90;
+            AA = 226.90;
             break;
-            case "Remedio5AA":
-            total =  total + 69,90;
-            AA = 69,90;
+            case "Colageno equaliv":
+            total =  total + 69.90;
+            AA = 69.90;
             break;
 
     }
 
     switch (selectC.value) {
-        case "Remedio1C":
-            total =  total + 9,90;
-            C = 9,90;
+        case "Magnesio complex":
+            total =  total + 9.90;
+            C = 9.90;
             break;
-            case "Remedio2C":
-            total =  total + 19,90;
-            C = 19,90;
+            case "Coenzima":
+            total =  total + 19.90;
+            C = 19.90;
             break;
-            case "Remedio3C":
-            total =  total + 169,59;
-            C = 169,59;
+            case "Andractiv":
+            total =  total + 169.59;
+            C = 169.59;
             break;
-            case "Remedio4C":
-            total =  total + 5,50;
-            C = 5,50;
+            case "DermUP":
+            total =  total + 5.50;
+            C = 5.50;
             break;
-            case "Remedio5C":
-            total =  total + 39,49;
-            C = 39,49;
+            case "20 bi":
+            total =  total + 39.49;
+            C = 39.49;
             break;
 
     }
 
     switch (selectAI.value) {
-        case "Remedio1AI":
-            total =  total + 139,99;
-            AI = 139,99;
+        case "Frauldapumpers":
+            total =  total + 139.99;
+            AI = 139.99;
             break;
-        case "Remedio2AI":
-            total =  total + 59,90;
-            AI = 59,90;
+        case "Enterogermina":
+            total =  total + 59.90;
+            AI = 59.90;
             break;
-        case "Remedio3AI":
-            total =  total + 75,90;
-            AI = 75,90;
+        case "Cicatricure":
+            total =  total + 75.90;
+            AI = 75.90;
         break;
-        case "Remedio4AI":
-            total =  total + 19,90;
-            AI = 19,90;
+        case "Agua micelar":
+            total =  total + 19.90;
+            AI = 19.90;
             break;
-        case "Remedio5AI":
-            total =  total + 49,90;
-            AI = 49,90;
+        case "Loreal Paris":
+            total =  total + 49.90;
+            AI = 49.90;
         break;
 
     }
     switch (selectD.value) {
-        case "Remedio1D":
-            total =  total + 48,90;
-            D = 48,90;
+        case "Doralgina":
+            total =  total + 48.90;
+            D = 48.90;
             break;
-        case "Remedio2D":
-            total =  total + 34,90;
-            D = 34,90;
+        case "Lavitan":
+            total =  total + 34.90;
+            D = 34.90;
             break;
-        case "Remedio3D":
-            total =  total + 35,90;
-            D = 35,90;
+        case "Atroveran":
+            total =  total + 35.90;
+            D = 35.90;
         break;
-        case "Remedio4D":
-            total =  total + 7000,90;
-            D = 7000,90;
+        case "Medley Dipirona":
+            total =  total + 7000.90;
+            D = 7000.90;
             break;
-        case "Remedio5D":
-            total =  total + 1,90;
-            D = 1,90;
+        case "Prati paracetamol":
+            total =  total + 1.90;
+            D = 1.90;
         break;
 
     }
     switch (selectI.value) {
-        case "Remedio1I":
-            total =  total + 62,90;
-            I = 62,90;
+        case "Deltametril":
+            total =  total + 62.90;
+            I = 62.90;
             break;
-        case "Remedio2I":
-            total =  total +29,90;
-            I = 29,90;
+        case "Suprabio":
+            total =  total +29.90;
+            I = 29.90;
             break;
-        case "Remedio3I":
-            total =  total + 47,48;
-            I = 47,48;
+        case "Nevralgex":
+            total =  total + 47.48;
+            I = 47.48;
         break;
-        case "Remedio4I":
-            total =  total + 116,80;
-            I = 116,80;
+        case "Sanasar":
+            total =  total + 116.80;
+            I = 116.80;
             break;
-        case "Remedio5I":
-            total =  total + 9,56;
-            I = 9,56;
+        case "vitaminas neo química":
+            total =  total + 9.56;
+            I = 9.56;
         break;
 
     }
@@ -159,6 +164,7 @@ function calcularTotal(){
     document.getElementById("RemedioD-pronto").textContent = selectD.value + " R$ " + D.toFixed(2); 
     document.getElementById("RemedioI-pronto").textContent = selectI.value + " R$ " + I.toFixed(2);  
     resultadoSpan.textContent = "R$" + total.toFixed(2);
+    totalGlobal = "R$" + total.toFixed(2);
 }
 
 function limpar(){
@@ -171,4 +177,22 @@ function limpar(){
     document.getElementById("select-anti-inflamatorio").value = "Selecione seu medicamento";
     document.getElementById("select-Dores").value = "Selecione seu medicamento";
     document.getElementById("select-Inchaco").value = "Selecione seu medicamento";
+}
+
+const DataAtual = new Date();
+
+const dia = DataAtual.getDate();
+const mes = DataAtual.getMonth() + 1;
+const ano = DataAtual.getFullYear();
+const data = dia + "/" + mes + "/" + ano
+
+function enviar(){
+    if (totalGlobal != ""){
+
+        var numeroTelefone = "5541991760161";
+
+        var linkWhatsApp = "https://wa.me/" + numeroTelefone + "?text=Total da Compra: " + totalGlobal + " - " + "Dia da Compra: " + data
+
+        window.open(linkWhatsApp, "_blank")
+    }
 }
